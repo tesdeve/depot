5fc7868584f48e2c61476ca97c3dddbe71de8015
tesdeve@gmail.com <tesdeve@gmail.com>
2021-08-03 13:18:45 +0100

    Finds a Cart and Connects products to cart

--- app/models/product.rb
+++ app/models/product.rb
@@ -9,4 +9,17 @@ class Product < ApplicationRecord
     with: %r{\.(gif|jpg|png)\z}i,
     message: 'must be a URL for GIF, JPG or PNG image.'
   }
+
+  has_many :line_items, dependent: :destroy
+  
+  before_destroy :ensure_not_referenced_by_any_line_item
+
+  private
+  # ensure that there are no line items referencing this product
+  def ensure_not_referenced_by_any_line_item
+    unless line_items.empty?
+      errors.add(:base, 'Line Items present')
+      throw :abort
+    end
+  end
 end
--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -55,6 +55,15 @@ end
     assert_redirected_to product_url(@product)
   end
 
+
+  test 'can delete product in a cart' do 
+    assert_difference('Product.count', 0) do
+      delete product_url(products(:two))
+    end
+
+    assert_redirected_to products_url      
+  end
+
   test "should destroy product" do
     assert_difference('Product.count', -1) do
       delete product_url(@product)
@@ -62,4 +71,10 @@ end
 
     assert_redirected_to products_url
   end
+
+
+
+
+
+  #end 
 end
